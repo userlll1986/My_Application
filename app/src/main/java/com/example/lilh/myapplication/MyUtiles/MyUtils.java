3700a9633b1a68e3c4b77801535e53d48f4e899c
lilh <lilinhu2008@qq.com>
2016-03-02 16:23:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lilh/myapplication/MyUtiles/MyUtils.java
@@ -0,0 +1,10 @@
+package com.example.lilh.myapplication.MyUtiles;
+
+/**
+ * Created by Administrator on 16-3-1.
+ */
+public class MyUtils {
+    public  String getValue(){
+        return "i am tom.";
+    }
+}
